a2b3f3b0fb8a782082ebdd891fda55c353c0bd13
MarcRuizenti <146344001+MarcRuizenti@users.noreply.github.com>
2024-02-09 13:01:20 +0100

    Echo

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -9,8 +9,15 @@ public class UI : MonoBehaviour
     public TMP_Text Puntuacion;
     public TMP_Text Vidas;
     public GameObject win;
+    public GameObject gameOver;
     public float vidas;
     public float puntuacion;
+    private BallManager ballManager;
+
+    private void Start()
+    {
+        ballManager = FindObjectOfType<BallManager>();
+    }
     void Update()
     {
         Vidas.text = vidas.ToString();
@@ -18,6 +25,19 @@ public class UI : MonoBehaviour
         if (puntuacion >= 210)
         {
             win.SetActive(true);
+            foreach (GameObject ball in ballManager.balls)
+            { 
+                Destroy(ball);
+            }
+            Time.timeScale = 0;
+        }
+        else if (vidas <= 0)
+        { 
+            gameOver.SetActive(true);
+            foreach (GameObject ball in ballManager.balls)
+            {
+                Destroy(ball);
+            }
             Time.timeScale = 0;
         }
     }
